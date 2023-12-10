08e115bd6c89809f4ee1b73534ca132fd0cb914a
RAVI KUMAR YEDLA <84285118+R807989@users.noreply.github.com>
2023-12-10 15:19:47 +0530

    Update settings.py

--- HRMS-Server/mHRMS/settings.py
+++ HRMS-Server/mHRMS/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key_here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [3.86.88.28]
+ALLOWED_HOSTS = ["3.86.88.28"]
 
 AUTH_USER_MODEL = "AdminAccount.User"
 
