d15ed53a12bcb5e5121c42b260c196cec67acabf
wajid8688 <138762135+wajid8688@users.noreply.github.com>
2023-12-13 08:29:31 +0530

    Update settings.py

--- HRMS-Server/mHRMS/settings.py
+++ HRMS-Server/mHRMS/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key_here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.169.84.139"]
 
 AUTH_USER_MODEL = "AdminAccount.User"
 
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.sessions',"
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AdminAccount',
